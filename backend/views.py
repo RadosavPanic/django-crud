eae535f6eb9975d191568c1b346a82b38a48b238
RadosavPanic <radosav.panic@gmail.com>
2025-12-02 15:52:37 +0100

    added task update for parsing form data

--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from . import models
 
 def home(request):
     return render(request, 'signup.html')
 
 def signup(request):
-    # if request.method == "POST":
-    #     fnm =
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        print(f"Username: {username}, Email: {email}, Password: {password}")
     return render(request, 'signup.html')
